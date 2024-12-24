73105b7082e7600d3398aa5338d4a8ed67644fe8
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-12-24 15:09:32 -0800

    Product Detail Page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,12 +7,14 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       {/* Add more routes here */}
        <Route index={true} path="/" element={<HomeScreen/>} /> 
+       <Route path="/product/:id" element={<ProductScreen/>} /> 
     </Route>
   )
 );
