f18ba33b8c196d2356a9024931dbbc175502ae47
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-12-25 08:29:32 -0800

    Fetching products from backend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,23 +1,24 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import {Row, Col} from 'react-bootstrap'
-import products from '../products'
 import ProductCard from '../Components/ProductCard'
 
 function HomeScreen() {
+  const [products, setProducts] = useState([])
+  
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const {data} = await axios.get('/api/products')
+      setProducts(data)
+    }
+    fetchProducts()
+  },[])
   return (
     <>
     <h1>Latest Products </h1>
     <Row>
         {products.map(product => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}> 
-            {/* You can pass the product detials directly here just like i did and you can also create a productCard component and import it anywheere you want to use it like i imported it here
-            <div className="card">
-              <img src={product.image} alt={product.name} />
-              <div className="card-body">
-                <h3 className="card-title">{product.name}</h3>
-                <p className="card-text">{product.description}</p>
-                <h6>${product.price}</h6>
-              </div>
-            </div> */}
             <ProductCard product={product} />
           </Col>
         ))}
