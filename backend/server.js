8f7c23d5581d9d08825124d16391dcd5af75c92e
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-12-25 03:16:48 -0800

    Backend setup, express server and initial Product route

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import products from './data/products.js';
+
+const port = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, World! My API is running');
+})
+
+//Route to get all products
+app.get('/api/products', (req, res) => {
+    res.json(products);
+})
+
+//Route to get a single product
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+})
+
+
+app.listen(port, ()=> console.log(`App running on port ${port}`));
