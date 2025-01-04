c4bc87f5fe0f18b73cd5ea7b72560be3d5745b28
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-01-04 15:19:20 -0800

    SHOPPING CART FUNCTIONALITY : Cart Slice and Reducer.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import { apiSlice } from './slices/apiSlice';
+import cartSliceReducer from './slices/cartSlice';
 
 const store = configureStore({
     reducer: {
         // Define your reducers here (e.g., { counter: counterReducer })
-        [apiSlice.reducerPath]: apiSlice.reducer
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        cart: cartSliceReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware), 
     devTools: true,
