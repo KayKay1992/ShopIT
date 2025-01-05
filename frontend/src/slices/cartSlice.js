10ef457943de9d3d21b7f78b377756512b62c641
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-01-04 16:04:02 -0800

    Adding Add to the cart functionality

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,10 +2,44 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {cartItems: []};
 
+const addDecimals = (num) => {
+    return (Math.round(num *100)/100).toFixed(2)
+}
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers: {}
+    reducers: {
+        addToCart: (state, action) => {
+            const item = action.payload;
+
+
+            const existItem = state.cartItems.find((x) => x._id === item._id)
+
+            if (existItem){
+                state.cartItems = state.cartItems.map((x) =>x._id === existItem._id ? item : x);
+            }else{
+                state.cartItems = [...state.cartItems, item]
+            }
+            //calculate the total i.e the items price, tax and shipping charges.
+
+            //calculate the items price
+            state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
+
+            //calculate the total shipping charges(if order is over $100 then free shipping charges otherwise it will be $10 charge)
+            state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
+
+            //calculate the total tax(15% tax)
+            state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
+
+            //calculate the total price.
+            state.totalPrice = addDecimals(Number(state.itemsPrice) + Number(state.taxPrice) + Number(state.shippingPrice));
+
+            //Saving our cart in localstorage.
+            localStorage.setItem('cart', JSON.stringify(state));
+        }
+    }
 })
+
+export const {addToCart} = cartSlice.actions
 
 export default cartSlice.reducer
