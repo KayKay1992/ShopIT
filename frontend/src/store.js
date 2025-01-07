f00935b37e04778c15d78cec91b99ea16fd49b3f
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-01-07 08:55:02 -0800

    Frontend End Authentication. Auth slice and user API Slice.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit'
 import { apiSlice } from './slices/apiSlice';
 import cartSliceReducer from './slices/cartSlice';
+import authSliceReducer from './slices/authSlice';
 
 const store = configureStore({
     reducer: {
         // Define your reducers here (e.g., { counter: counterReducer })
         [apiSlice.reducerPath]: apiSlice.reducer,
         cart: cartSliceReducer,
+        auth: authSliceReducer,  // Add your authslice 
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware), 
     devTools: true,
