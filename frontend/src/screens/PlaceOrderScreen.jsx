0bcab71a188ed26cd44cacf2b9ff4d4d28e8ba57
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-01-12 02:06:54 -0800

    Creating Order and setting the ui

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,29 +1,153 @@
-import { useEffect } from "react"
-import {Link, useNavigate} from 'react-router-dom'
-import { useSelector, } from "react-redux"
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 import { Button, Row, Image, Card, ListGroup, Col } from "react-bootstrap";
 import CheckoutSteps from "../Components/CheckoutSteps";
+import Message from "../Components/Message";
+import Loader from "../Components/Loader";
+import { useCreateOrderMutation } from "../slices/ordersApiSlice";
+import { clearCartItems } from "../slices/cartSlice";
 
 function PlaceOrderScreen() {
-    const navigate = useNavigate();
-    const cart = useSelector((state) => state.cart)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
 
-    useEffect(() =>{
-        if(!cart.shippingAddress.address) {
-            navigate("/shipping");
-        }else if(!cart.paymentMethod){
-            navigate("/payment");
-        }
-    }, [cart.paymentMethod, cart.shippingAddress.address, navigate]);
+  const [createOrder, { isLoading, error }] = useCreateOrderMutation();
+
+  useEffect(() => {
+    if (!cart.shippingAddress.address) {
+      navigate("/shipping");
+    } else if (!cart.paymentMethod) {
+      navigate("/payment");
+    }
+  }, [cart.paymentMethod, cart.shippingAddress.address, navigate]);
+
+  const placeOrderHandler = async  () => {
+    try{
+      const order = await createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      }).unwrap();
+      dispatch(clearCartItems());
+      navigate(`/order/${order._id}`);
+      toast.success("Order placed successfully!");
+    }catch(error){
+      toast.error("Error placing order: " + error.message);
+    }
+  };
+
+  
   return (
     <>
-    <CheckoutSteps step1 step2 step3 step4 />
+      <CheckoutSteps step1 step2 step3 step4 />
       <Row>
-        <Col md={8}>Column</Col>
-        <Col md={4}>Column</Col>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping </h2>
+              <p>
+                <stong>Address:</stong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city},{" "}
+                {cart.shippingAddress.postalCode},{" "}
+                {cart.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <strong>Method : </strong>
+              {cart.paymentMethod}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {cart.cartItems.length === 0 ? (
+                <Message> Your Cart is Empty</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={2}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                            style={{ height: "50px" }}
+                          />
+                        </Col>
+                        <Col md={6}>
+                          <Link to={`/product/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x ${item.price}= ${item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    <strong>Items:</strong> ${cart.itemsPrice}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    <strong>Shipping:</strong> ${cart.shippingPrice}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    <strong>Tax:</strong> ${cart.taxPrice}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    <strong>Total:</strong> ${cart.totalPrice}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+              {error && <Message variant="danger">{error.message}</Message>}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button type="button" onClick={placeOrderHandler} disabled={cart.cartItems.length === 0}>
+                  Place Order
+                </Button>
+                {isLoading && <Loader />}
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
       </Row>
     </>
-  )
+  );
 }
 
-export default PlaceOrderScreen
+export default PlaceOrderScreen;
